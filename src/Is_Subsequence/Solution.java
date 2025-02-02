71e0d55b4b9d76abb687838e37a03c69987e7419
AnithaReddy96 <66546373+AnithaReddy96@users.noreply.github.com>
2025-02-02 18:45:33 +0530

    problems

---
+++ src/Is_Subsequence/Solution.java
@@ -0,0 +1,41 @@
+package Is_Subsequence;
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+ */
+public class Solution {
+    public static boolean isSubsequence(String s, String t){
+        int left =0;
+        int right =0;
+        int count=0;
+
+        while(left<s.length() && right<t.length()){
+            if(s.charAt(left)==t.charAt(right)){
+                left++;
+                right++;
+                count++;
+            }else{
+                right++;
+            }
+        }
+        return s.length()==count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isSubsequence("abc","ahbgdc"));
+        System.out.println(isSubsequence("axc","ahbgdc"));
+    }
+}
---
+++ src/Valid_Palindrome/Solution.java
@@ -0,0 +1,54 @@
+package Valid_Palindrome;
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+ */
+public class Solution {
+
+    public static boolean isPalindrome(String s){
+        s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int left =0;
+        int right =s.length()-1;
+
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(isPalindrome("race a car"));
+        System.out.println(isPalindrome(" "));
+    }
+}
